d91fc4bc77491dff2dfa7592463001fda571fdb6
mpol <mpol@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-28 15:04:41 +0000

    Tag 1.5.9
    git-svn-id: https://plugins.svn.wordpress.org/related/tags/1.5.9@1101877 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- scripts.js
+++ scripts.js
@@ -1,25 +1,87 @@
+
+
 jQuery(document).ready(function($) {
-	
-	$('#related-posts-select').change(function() {
+
+	/*
+	 * On selection (change event), add the post to the list in the metabox
+	 */
+
+	$('.related-posts-select').change(function() {
 		var select = $(this),
 				container = $('#related-posts'),
 				id = select.val(),
 				title = this.options[this.options.selectedIndex].text;
-		
-		if ($('#related-post-' + id).length == 0) {
-			container.prepend('<div class="related-post" id="related-post-' + id + '"><input type="hidden" name="related-posts[]" value="' + id + '"><span class="related-post-title">' + title + '</span><a href="#">Delete</a></div>');
+
+		if (id != "0") {
+			if ($('#related-post-' + id).length == 0) {
+				container.prepend('<div class="related-post" id="related-post-' +
+									id +
+									'"><input type="hidden" name="related-posts[]" value="' +
+									id +
+									'"><span class="related-post-title">' +
+									title +
+									'</span><a href="#" onClick="related_delete( this ); return false;">Delete</a></div>'
+								);
+			}
 		}
 	});
-	
-	$('.related-post a').live('click', function() {
-		var div = $(this).parent();
-		
-		div.css('background-color', '#ff0000').fadeOut('normal', function() {
-			div.remove();
-		});
+
+	/* Delete option again on click event */
+
+	$('.related-post a').on('click', function() {
+		related_delete( this );
+		return false;
+
+	});
+
+	$('#related-posts').sortable();
+
+});
+
+/*
+ * related_delete
+ * Function te remove the selected post
+ */
+
+function related_delete( a_el ) {
+	var div = jQuery( a_el ).parent();
+
+	div.css('background-color', '#ff0000').fadeOut('normal', function() {
+		div.remove();
+	});
+	return false;
+}
+
+
+/*
+ * Select the right tab on the options page
+ *
+ */
+jQuery(document).ready(function($) {
+	jQuery( '.related-nav-tab-wrapper a' ).on('click', function() {
+
+		jQuery( '.related_options' ).removeClass( 'active' );
+		jQuery( '.related-nav-tab-wrapper a' ).removeClass( 'nav-tab-active' );
+
+		var rel = jQuery( this ).attr('rel');
+		jQuery( '.' + rel ).addClass( 'active' );
+		jQuery( this ).addClass( 'nav-tab-active' );
+
 		return false;
 	});
-	
-	$('#related-posts').sortable();		
-	
 });
+
+
+/*
+ * Use Chosen.js to limit the number of shown options in the select-box
+ *
+ */
+
+jQuery(document).ready(function($) {
+	$('select.related-posts-select').chosen({
+		no_results_text: "Nothing found...",
+		allow_single_deselect: true,
+		width: "100%"
+	});
+});
+
