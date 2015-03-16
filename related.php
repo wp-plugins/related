eeb1ac479ae5adf1b8c35db923f6f616ce85158c
mpol <mpol@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-16 12:32:44 +0000

    Tag 1.6.1
    git-svn-id: https://plugins.svn.wordpress.org/related/tags/1.6.1@1113891 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- related.php
+++ related.php
@@ -1,14 +1,17 @@
 <?php
 /*
 Plugin Name: Related
-Plugin URI: https://github.com/matthiassiegel/Related
-Description: A simple 'related posts' plugin that lets you select related posts manually instead of automatically generating the list.
-Version: 1.1.1
-Author: Matthias Siegel
-Author URI: https://github.com/matthiassiegel/Related
+Plugin URI: http://products.zenoweb.nl/free-wordpress-plugins/related/
+Description: A simple 'related posts' plugin that lets you select related posts manually.
+Version: 1.6.1
+Author: Marcel Pol
+Author URI: http://zenoweb.nl
+Text Domain: related
+Domain Path: /lang/
 
 
 Copyright 2010-2012  Matthias Siegel  (email: matthias.siegel@gmail.com)
+Copyright 2013-2015  Marcel Pol       (email: marcel@timelord.nl)
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -30,188 +33,347 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 if (!class_exists('Related')) :
 	class Related {
 
-		// Constructor
+		/*
+		 * __construct
+		 * Constructor
+		 */
 		public function __construct() {
-			
+
 			// Set some helpful constants
 			$this->defineConstants();
-						
+
 			// Register hook to save the related posts when saving the post
 			add_action('save_post', array(&$this, 'save'));
 
 			// Start the plugin
 			add_action('admin_menu', array(&$this, 'start'));
+
+			// Add the related posts to the content, if set in options
+			add_filter( 'the_content', array($this, 'related_content_filter') );
 		}
-		
 
-		// Defines a few static helper values we might need
+
+		/*
+		 * defineConstants
+		 * Defines a few static helper values we might need
+		 */
 		protected function defineConstants() {
-
-			define('RELATED_VERSION', '1.1.1');
-			define('RELATED_HOME', 'https://github.com/matthiassiegel/Related');
+			define('RELATED_VERSION', '1.6.1');
+			define('RELATED_HOME', 'http://zenoweb.nl');
 			define('RELATED_FILE', plugin_basename(dirname(__FILE__)));
 			define('RELATED_ABSPATH', str_replace('\\', '/', WP_PLUGIN_DIR . '/' . plugin_basename(dirname(__FILE__))));
 			define('RELATED_URLPATH', WP_PLUGIN_URL . '/' . plugin_basename(dirname(__FILE__)));
 		}
-				
-		
-		// Main function
+
+
+		/*
+		 * start
+		 * Main function
+		 */
 		public function start() {
-			
+
 			// Load the scripts
 			add_action('admin_print_scripts', array(&$this, 'loadScripts'));
-			
+
 			// Load the CSS
 			add_action('admin_print_styles', array(&$this, 'loadCSS'));
-			
+
 			// Adds a meta box for related posts to the edit screen of each post type in WordPress
-			foreach (get_post_types() as $post_type) :
-				add_meta_box($post_type . '-related-posts-box', 'Related posts', array(&$this, 'displayMetaBox'), $post_type, 'normal', 'high');
-			endforeach;
+			$related_show = get_option('related_show');
+			$related_show = json_decode( $related_show );
+			if ( empty( $related_show ) ) {
+				$related_show = array();
+				$related_show[] = 'any';
+			}
+			if ( in_array( 'any', $related_show ) ) {
+				foreach (get_post_types() as $post_type) :
+					add_meta_box($post_type . '-related-posts-box', __('Related posts', 'related' ), array(&$this, 'displayMetaBox'), $post_type, 'normal', 'high');
+				endforeach;
+			} else {
+				foreach ($related_show as $post_type) :
+					add_meta_box($post_type . '-related-posts-box', __('Related posts', 'related' ), array(&$this, 'displayMetaBox'), $post_type, 'normal', 'high');
+				endforeach;
+			}
+
 		}
 
 
-		// Load Javascript
+		/*
+		 * loadScripts
+		 * Load Javascript
+		 */
 		public function loadScripts() {
-		
 			wp_enqueue_script('jquery-ui-core');
 			wp_enqueue_script('jquery-ui-sortable');
 			wp_enqueue_script('related-scripts', RELATED_URLPATH .'/scripts.js', false, RELATED_VERSION);
+			wp_enqueue_script('related-chosen', RELATED_URLPATH .'/chosen/chosen.jquery.min.js', false, RELATED_VERSION);
 		}
 
 
-		// Load CSS
+		/*
+		 * loadCSS
+		 * Load CSS
+		 */
 		public function loadCSS() {
-		
 			wp_enqueue_style('related-css', RELATED_URLPATH .'/styles.css', false, RELATED_VERSION, 'all');
+			wp_enqueue_style('related-css-chosen', RELATED_URLPATH .'/chosen/chosen.min.css', false, RELATED_VERSION, 'all');
 		}
 
 
-		// Save related posts when saving the post
+		/*
+		 * save
+		 * Save related posts when saving the post
+		 */
 		public function save($id) {
-			
-			global $wpdb;
-			
+			global $pagenow;
+
 			if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) return;
 
-			if (!isset($_POST['related-posts']) || empty($_POST['related-posts'])) :
-				delete_post_meta($id, 'related_posts');
-			else :
+			if ( isset($_POST['related-posts']) ) {
 				update_post_meta($id, 'related_posts', $_POST['related-posts']);
-			endif;			
+			}
+			/* Only delete on post.php page, not on Quick Edit. */
+			if ( empty($_POST['related-posts']) ) {
+				if ( $pagenow == 'post.php' ) {
+					delete_post_meta($id, 'related_posts');
+				}
+			}
 		}
 
 
-		// Creates the output on the post screen
+		/*
+		 * displayMetaBox
+		 * Creates the output on the post screen
+		 */
 		public function displayMetaBox() {
-			
 			global $post;
-			
+
 			$post_id = $post->ID;
-			
-			echo '<div id="related-posts">';
-			
+
+			echo '<p>' . __('Choose related posts. You can drag-and-drop them into the desired order:', 'related' ) . '</p><div id="related-posts">';
+
 			// Get related posts if existing
 			$related = get_post_meta($post_id, 'related_posts', true);
 
 			if (!empty($related)) :
 				foreach($related as $r) :
 					$p = get_post($r);
+
+
 					echo '
 						<div class="related-post" id="related-post-' . $r . '">
+
+
 							<input type="hidden" name="related-posts[]" value="' . $r . '">
 							<span class="related-post-title">' . $p->post_title . ' (' . ucfirst(get_post_type($p->ID)) . ')</span>
-							<a href="#">Delete</a>
+							<a href="#">' . __('Delete', 'related' ) . '</a>
 						</div>';
 				endforeach;
 			endif;
-			
+
+			/* First option should be empty with a data placeholder for text.
+			 * The jQuery call allow_single_deselect makes it possible to empty the selection
+			 */
 			echo '
 				</div>
 				<p>
-					<select id="related-posts-select" name="related-posts-select">
-						<option value="0">Select</option>';
-			
-			$query = array(
-				'nopaging' => true,
-				'post__not_in' => array($post_id),
-				'post_status' => 'publish',
-				'posts_per_page' => -1,
-				'post_type' => 'any',
-				'orderby' => 'title',
-				'order' => 'ASC'
-			);
-			
-			$p = new WP_Query($query);
-			
-			if ($p->have_posts()) :
-				while ($p->have_posts()) :
-					$p->the_post();
-					echo '
-						<option value="' . get_the_ID() . '">' . get_the_title() . ' (' . ucfirst(get_post_type(get_the_ID())) . ')</option>';
-				endwhile;
-			endif;
-			
+					<select class="related-posts-select chosen-select" name="related-posts-select" data-placeholder="' . __('Choose a related post... ', 'related' ) . '">';
+
+			echo '<option value="0"></option>';
+
+
+			$related_list = get_option('related_list');
+			$related_list = json_decode( $related_list );
+
+			if ( empty( $related_list ) ) {
+				$related_list = array();
+				$related_list[] = 'any';
+			}
+
+
+			/*
+			 * If in Settings 'any' is set it will just list the options in the select-box.
+			 * If specific posttypes are set, it will show each posttype in an optgroup in the select-box.
+			 * Also fetch attachments by setting post_status to 'inherit' as well.
+			 */
+
+			if ( in_array( 'any', $related_list ) ) {
+
+				$query = array(
+					'nopaging' => true,
+					'post__not_in' => array($post_id),
+					'post_status' => 'publish, inherit',
+					'posts_per_page' => -1,
+					'post_type' => 'any',
+					'orderby' => 'title',
+					'order' => 'ASC'
+				);
+				$p = new WP_Query($query);
+
+				foreach ($p->posts as $thePost) {
+					?>
+					<option value="<?php echo $thePost->ID; ?>">
+						<?php echo $thePost->post_title . ' (' . ucfirst(get_post_type($thePost->ID)) . ')'; ?>
+					</option>
+					<?php
+				}
+
+			} else {
+
+				foreach ( $related_list as $post_type ) {
+
+					$query = array(
+						'nopaging' => true,
+						'post__not_in' => array($post_id),
+						'post_status' => 'publish, inherit',
+						'posts_per_page' => -1,
+						'post_type' => $post_type,
+						'orderby' => 'title',
+						'order' => 'ASC'
+					);
+					$p = new WP_Query($query);
+
+					echo '<optgroup label="'. $post_type .'">';
+
+						foreach ($p->posts as $thePost) {
+							?>
+							<option value="<?php echo $thePost->ID; ?>">
+								<?php echo  $thePost->post_title; ?>
+							</option>
+							<?php
+						}
+
+					echo '</optgroup>';
+
+				}
+
+			}
 			wp_reset_query();
 			wp_reset_postdata();
-								
+
 			echo '
 					</select>
-				</p>
-				<p>
-					Select related posts from the list. Drag selected ones to change order.
 				</p>';
+
 		}
 
 
-		// The frontend function that is used to display the related post list
-		public function show($id, $return = false) {
+		/*
+		 * show
+		 * The frontend function that is used to display the related post list
+		 */
+		public function show( $id, $return = false ) {
 
 			global $wpdb;
 
+			/* Compatibility for Qtranslate, Qtranslate-X and MQtranslate, and the get_permalink function */
+			$plugin = "qtranslate/qtranslate.php";
+			$q_plugin = "qtranslate-x/qtranslate.php";
+			$m_plugin = "mqtranslate/mqtranslate.php";
+			include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+			if ( is_plugin_active($plugin) || is_plugin_active($q_plugin) || is_plugin_active($m_plugin) ) {
+				add_filter('post_type_link', 'qtrans_convertURL');
+			}
+
 			if (!empty($id) && is_numeric($id)) :
 				$related = get_post_meta($id, 'related_posts', true);
-				
+
 				if (!empty($related)) :
 					$rel = array();
 					foreach ($related as $r) :
 						$p = get_post($r);
 						$rel[] = $p;
 					endforeach;
-					
+
 					// If value should be returned as array, return it
 					if ($return) :
 						return $rel;
-						
+
 					// Otherwise return a formatted list
 					else :
-						$list = '<ul class="related-posts">';
-						foreach ($rel as $r) :
-							$list .= '<li><a href="' . get_permalink($r->ID) . '">' . $r->post_title . '</a></li>';
-						endforeach;
-						$list .= '</ul>';
-						
-						return $list;
+						if ( is_array( $rel ) && count( $rel ) > 0 ) {
+							$list = '<ul class="related-posts">';
+							foreach ($rel as $r) :
+								if ( is_object( $r ) ) {
+									if ($r->post_status != 'trash') {
+										$list .= '<li><a href="' . get_permalink($r->ID) . '">' . get_the_title($r->ID) . '</a></li>';
+									}
+								}
+							endforeach;
+							$list .= '</ul>';
+
+							return $list;
+						}
 					endif;
 				else :
 					return false;
 				endif;
 			else :
-				return 'Invalid post ID specified';
+				return __('Invalid post ID specified', 'related' );
 			endif;
 		}
+
+
+		/*
+		 * Add the plugin data to the content, if it is set in the options.
+		 */
+		public function related_content_filter( $content ) {
+			if ( get_option( 'related_content', 0 ) == 1 && is_single() ) {
+				global $related;
+				$related_posts = $related->show( get_the_ID() );
+				if ( $related_posts ) {
+					$content .= '<div class="related_content" style="clear:both;">';
+					$content .= '<h3 class="widget-title">';
+					$content .= get_option('related_content_title', __('Related Posts', 'related'));
+					$content .= '</h3>';
+					$content .= $related_posts;
+					$content .= "</div>";
+				}
+			}
+			// otherwise returns the old content
+			return $content;
+		}
+
 	}
-	
-	
-	
+
 endif;
 
 
+/*
+ * related_links
+ * Add Settings link to the main plugin page
+ *
+ */
 
-// Start the plugin
+function related_links( $links, $file ) {
+	if ( $file == plugin_basename( dirname(__FILE__).'/related.php' ) ) {
+		$links[] = '<a href="' . admin_url( 'options-general.php?page=related.php' ) . '">'.__( 'Settings', 'related' ).'</a>';
+	}
+	return $links;
+}
+add_filter( 'plugin_action_links', 'related_links', 10, 2 );
 
-global $related;
 
-$related = new Related();
+/* Include Settings page */
+include( 'page-related.php' );
 
-?>
+/* Include widget */
+include( 'related-widget.php' );
+
+
+/*
+ * related_init
+ * Function called at initialisation.
+ * - Loads language files
+ * - Make an instance of Related()
+ */
+
+function related_init() {
+	load_plugin_textdomain('related', false, dirname( plugin_basename( __FILE__ ) ) . '/lang/');
+
+	// Start the plugin
+	global $related;
+	$related = new Related();
+}
+add_action('plugins_loaded', 'related_init');
+
